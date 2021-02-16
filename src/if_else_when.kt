cd2da97ba767d74e0c3da0046977adc212933319
aleksandr <gavryukov@mail.ru>
2021-02-16 22:07:42 +0300

    if_else...

---
+++ src/if_else_when.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var s = 9
+
+    if(s == 10){
+        println("s = $s")
+    }
+    else {
+        println("s != $s")
+        s++
+        println("s теперь = $s")
+    }
+
+    when(x){
+        1 -> println(1)
+        2 -> println(2)
+        3 -> println(3)
+        else -> println("hello")
+    }
+
+}
