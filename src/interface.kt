5b4d09304dfe4fae379cdfef2fbb0088fd7d7345
aleksandr <gavryukov@mail.ru>
2021-02-25 22:13:11 +0300

    interfase_2

--- src/interface.kt
+++ src/interface.kt
@@ -1,16 +1,35 @@
 interface UserInfoProvider{
-    fun printInfo(user: my)
+    //значение переменной внутри интерфейса добавлять нельзя
+    val info: String
+
+    fun printInfo(user: my) {
+        //реализация функции внутри интерфейса
+        println(info)
+        //обращаемся к методу из класса user
+        user.printUser()
+    }
 }
 // абстрактный класс
 //abstract class MainProvider : UserInfoProvider{
 
 //}
 
+// создаем класс с интерфейсом UserInfoProvider
 class MainProvider : UserInfoProvider{
-    // переписываем функцию
+    //переписываем переменную
+    override val info: String
+        get() = "Method was called"
+//   // переписываем функцию из интерфейса UserInfoProvider
+//   override fun printInfo(user: my) {
+//       println("Message")
+//       //обращаемся к методу из класса user
+//       user.printUser()
+
+    // переписываем функцию из интерфейса UserInfoProvider
     override fun printInfo(user: my) {
-        println("Message")
-        //обращаемся к методу из класса user
-        user.printUser()
+        //обращение к функции в интерфейсе
+        super.printInfo(user)
+        println("дополнительный код")
     }
 }
+
